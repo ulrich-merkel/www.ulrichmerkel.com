1e2c1306c9700364cfacacffc931d107965069a6
Ulrich Merkel <hello@ulrichmerkel.com>
2016-11-07 00:47:27 +0100

    Improve tests for common/utils/content

---
+++ source/application/common/utils/__jest__/content.test.jsx
@@ -0,0 +1,44 @@
+/* eslint-disable func-names */
+/* global describe, it, expect */
+import { getContentSection, getTranslatedContent } from './../content';
+
+describe('content', function () {
+
+    it('should return a config via getContentSection', function () {
+        expect(getContentSection({
+            head: {
+                foo: 'bar'
+            }
+        })('head')).toEqual({foo: 'bar'});
+    });
+
+    it('should return a translated config via getTranslatedContent', function () {
+        const translatedContent = getTranslatedContent('en-EN', {
+            content: {
+                data: {
+                    foo: 'intl-test-1',
+                    bar: [
+                        'intl-test-2'
+                    ],
+                    foobar: 1,
+                    barfoo: 'no-translation-available'
+                }
+            },
+            'en-en': {
+                data: {
+                    'intl-test-1': 'test-1',
+                    'intl-test-2': 'test-2'
+                }
+            }
+        }, 'data');
+        expect(translatedContent).toEqual({
+            foo: 'test-1',
+            bar: [
+                'test-2'
+            ],
+            foobar: 1,
+            barfoo: 'no-translation-available'
+        });
+    });
+
+});
