a9eea02323814231d5d59ff1b01c87b5f344c9a4
Ulrich Merkel <hello@ulrichmerkel.com>
2016-11-07 01:35:35 +0100

    Improve tests for common/utils/transition

---
+++ source/application/common/utils/__jest__/transition.test.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable func-names */
+/* global describe, it, expect */
+import getSectionTransition from './../transition';
+import configApplication from './../../config/application';
+
+describe('transition', function () {
+
+    const transition = configApplication.transition;
+
+    it('should apply no transitionAppear if viewsAfterReload is 1', function () {
+        expect(getSectionTransition({
+            viewsAfterReload: 1
+        })).toEqual(Object.assign(
+            {},
+            transition,
+            {
+                transitionAppear: false
+            }
+        ))
+    });
+
+    it('should apply default transition if viewsAfterReload is greater than 1', function () {
+        expect(getSectionTransition({
+            viewsAfterReload: 2
+        })).toEqual(transition)
+    });
+
+});
