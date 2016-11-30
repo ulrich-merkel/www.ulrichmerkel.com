94f56fb173b32357a36d27a9f9ffe6557d68aad2
Ulrich Merkel <hello@ulrichmerkel.com>
2016-11-30 01:40:48 +0100

    Remove unused jest code

--- application/common/component/layout/__jest__/dialog.snapshot.test.jsx
+++ application/common/component/layout/__jest__/dialog.snapshot.test.jsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
-// import ReactTestUtils from 'react-addons-test-utils';
 
 import mockedStore from './../../__mocks__/store';
 import ComponentToBeTested from './../dialog';
@@ -10,7 +9,6 @@ import ComponentToBeTested from './../dialog';
 describe('component/layout/dialog', function () {
     describe('Snapshot', function () {
         const content = {};
-        // const handleChangeDialogVisible = jest.fn();
 
         it('should render correctly', function () {
             const tree = renderer.create(
@@ -22,33 +20,5 @@ describe('component/layout/dialog', function () {
             ).toJSON();
             expect(tree).toMatchSnapshot();
         });
-        // @TODO: FIXME: Handle events with jest
-        // it('should appear if needed', function () {
-        //     const jsx = (
-        //         <Provider store={mockedStore}>
-        //             <ComponentToBeTested
-        //                 content={content}
-        //                 dialogVisible={true}
-        //                 handleChangeDialogVisible={handleChangeDialogVisible}
-        //             >
-        //                 Hello
-        //             </ComponentToBeTested>
-        //         </Provider>
-        //     );
-        //     const component = renderer.create(
-        //         jsx
-        //     );
-        //
-        //     let tree = component.toJSON();
-        //     expect(tree).toMatchSnapshot();
-        //
-        //     const button = ReactTestUtils.findRenderedDOMComponentWithClass(jsx, 'l-dialog__button--close')
-        //
-        //     ReactTestUtils.Simulate.keyDown(button, {key: 'Esc', keyCode: 27, which: 27});
-        //     expect(handleChangeDialogVisible).toHaveBeenCalled();
-        //     tree = component.toJSON();
-        //     expect(tree).toMatchSnapshot();
-        //
-        // });
     });
 });
