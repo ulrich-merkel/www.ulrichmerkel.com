528d006c9f91eb6ae0d6d47b35f03b69ff751f6c
Ulrich Merkel <hello@ulrichmerkel.com>
2017-02-05 00:56:37 +0100

    Improve search filter index array creating

--- application/common/utils/search.jsx
+++ application/common/utils/search.jsx
@@ -1,31 +1,81 @@
-import { get, isArray, isObject, isString, flattenDeep } from 'lodash';
+/* eslint-disable import/prefer-default-export */
+import { get, isArray, isObject } from 'lodash';
 
+const PAGES = [
+    'PageIndex',
+    'PageWork',
+    'PageWorkOptikLudewig',
+    'PageWorkSummerInspiration',
+    'PageWorkMomentariness',
+    'PageWorkLebensweltSchule',
+    'PageWorkRevolution',
+    'PageWorkVerlegeserviceBunge',
+    'PageWorkGedankenKollektiv',
+    'PagePersona',
+    'PageContact',
+    'PageDisclaimer',
+    'PagePrivacy',
+    'PageImprint',
+    'PageBroadcast'
+];
+const CACHE = {};
 
-function flattenObjectToArray(object) {
-    return Object.keys(object).map(function (key) {
-        const value = object[key];
-
-        if (!isArray(value) && isObject(value)) {
-            return flattenObjectToArray(value);
-        }
-        if (isArray(value)) {
-            return flattenDeep(value)
-        }
-        return value
-    });
+function reducer(array, val) {
+    let value = val;
+    if (isObject(value)) {
+        value = Object.values(value);
+    }
+    if (isArray(value)) {
+        return value.reduce(reducer, array);
+    }
+    array.push(value);
+    return array;
 }
 
-// function getPagesByKey(configContent, key) {
-//     // return Object.keys(configContent).map(function (key) {
-//     //     console.log(flattenObjectToArray(configContent[key]))
-//     //     return key
-//     //     // reduce to simple structure
-//     //     // return object with page key and
-//     // // }).filter(function (key) {
-//     // //     //console.log(key)
-//     // //     // key === content key
-//     // });
-// }
+function reduce(object) {
+    return Object.values(object).reduce(reducer, []);
+}
+
+function createIndex(locale, config) {
+    const configContent = get(config, 'content.data', {});
+    const configTranslation = get(config, `${locale}.data`, {});
+
+    return PAGES.reduce(function (result, value) {
+        return Object.assign(
+            result,
+            {
+                [value]: Object.keys(configTranslation).filter(function (key) {
+                    return reduce(configContent[value]).includes(key);
+                })
+            }
+        );
+    }, {});
+}
+
+function translateIndex(locale, config, index) {
+    const configTranslation = get(config, `${locale}.data`, {});
+
+    return Object.keys(index).reduce(function (indexResult, indexKey) {
+        return Object.assign(
+            indexResult,
+            {
+                [indexKey]: index[indexKey].reduce(function (indexContent, configTranslationKey) {
+                    if (configTranslation[configTranslationKey]) {
+                        indexContent.push(configTranslation[configTranslationKey]);
+                    }
+                    return indexContent;
+                }, [])
+            }
+        );
+    }, {});
+}
+
+function getIndex(locale, config) {
+    if (!CACHE[locale]) {
+        CACHE[locale] = translateIndex(locale, config, createIndex(locale, config));
+    }
+    return CACHE[locale];
+}
 
 function findMatches(searchTerm, locale, config = {}) {
     const escapedInput = searchTerm && searchTerm.trim().toLowerCase();
@@ -34,23 +84,15 @@ function findMatches(searchTerm, locale, config = {}) {
     }
 
     const matchRegex = new RegExp(`\\b${escapedInput}`, 'i');
+    const index = getIndex(locale.toLowerCase(), config);
 
-    const configContent = get(config, 'content.data');
-    const configTranslation = get(config, `${locale.toLowerCase()}.data`);
-
-    console.log(flattenObjectToArray(configContent))
-    const suggestions = Object.keys(configTranslation)
+    return Object.keys(index)
         .filter(function (key) {
-            return matchRegex.test(configTranslation[key]);
-        })
-        .sort(function (key1, key2) {
-            return configTranslation[key1].indexOf(escapedInput) - configTranslation[key2].indexOf(escapedInput)
+            return matchRegex.test(index[key]);
         })
         .slice(0, 10);
-        // .map(getPagesByKey.bind(null, configContent))
-
 }
 
 export {
     findMatches
-}
+};
