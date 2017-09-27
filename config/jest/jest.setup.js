30cae7342c9fdb46282c28083a24e6cdbc848e7d
Ulrich Merkel <hello@ulrichmerkel.com>
2017-09-27 22:33:39 +0200

    Update dependencies: compression@1.7.1, helmet@3.8.2, morgan@1.9.0, react@16.0.0, react-dom@16.0.0, nsp@2.8.1, react-test-renderer@16.0.0, enzyme-adapter-react-16@1.0.0

--- config/jest/jest.setup.js
+++ config/jest/jest.setup.js
@@ -1,3 +1,8 @@
-'use strict';
-
 require('babel-polyfill');
+const configure = require('enzyme').configure;
+const Adapter = require('enzyme-adapter-react-16');
+
+// Configure enzyme, see {@link http://airbnb.io/enzyme/docs/installation/react-15.html}
+configure({
+    adapter: new Adapter()
+});
