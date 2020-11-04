a2bf9b26fd85e79cdfff15b1cb2e8f752f28ac2e
Ulrich Merkel <hello@ulrichmerkel.com>
2020-11-04 14:11:40 +0100

    Adjust yarn lock file

--- application/client/bootstrap.jsx
+++ application/client/bootstrap.jsx
@@ -18,33 +18,33 @@
  * - 0.0.2 Added service workers
  * - 0.0.1 Basic functions and structure
  */
-// import configApplication from '../common/config/application';
+import configApplication from '../common/config/application';
 import './loader/offline';
 import loaderAsync from './loader/async';
-// import { displayAllLoaded } from './loader/progress-bar';
+import { displayAllLoaded } from './loader/progress-bar';
 
 // Register the service worker if available
-// if (configApplication.serviceWorker.use && navigator.serviceWorker) {
-//     navigator.serviceWorker.register('/service-worker.bundle.js')
-//         .then(function registerServiceWorker(reg) {
-//             const { log } = console; // eslint-disable-line no-console
-//             displayAllLoaded();
-//
-//             if (reg.waiting) {
-//                 return log('Service worker installed');
-//             }
-//             if (reg.installing) {
-//                 return log('Service worker installing');
-//             }
-//             if (reg.active) {
-//                 return log('Service worker active');
-//             }
-//             return log('Successfully registered service worker');
-//         })
-//         .catch(function catchRegisterServiceWorker(err) {
-//             console.warn('Error whilst registering service worker', err); // eslint-disable-line no-console
-//         });
-// }
+if (configApplication.serviceWorker.use && navigator.serviceWorker) {
+    navigator.serviceWorker.register('/service-worker.bundle.js')
+        .then(function registerServiceWorker(reg) {
+            const { log } = console; // eslint-disable-line no-console
+            displayAllLoaded();
+
+            if (reg.waiting) {
+                return log('Service worker installed');
+            }
+            if (reg.installing) {
+                return log('Service worker installing');
+            }
+            if (reg.active) {
+                return log('Service worker active');
+            }
+            return log('Successfully registered service worker');
+        })
+        .catch(function catchRegisterServiceWorker(err) {
+            console.warn('Error whilst registering service worker', err); // eslint-disable-line no-console
+        });
+}
 
 // Load assets async to improve performance
 loaderAsync.css({
