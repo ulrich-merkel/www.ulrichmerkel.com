f859f86e4e70a0a767a17f224cccb42e54006176
Ulrich Merkel <hello@ulrichmerkel.com>
2020-11-05 13:45:43 +0100

    Add class properties for babel

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable immutable/no-mutation */
 module.exports = {
     include: [/\.(js|jsx|ts|tsx)$/],
+    plugins: ['@babel/plugin-proposal-class-properties'],
     presets: [
         [
             '@babel/preset-react',
