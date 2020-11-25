0af03388cef4750e92aa57c1821426a2324c0a35
Ulrich Merkel <hello@ulrichmerkel.com>
2020-11-25 23:36:17 +0100

    Fix unit tests

--- application/common/utils/__tests__/function.test.ts
+++ application/common/utils/__tests__/function.test.ts
@@ -1,15 +1,10 @@
 import { callFn } from '../function';
 
-describe('isValidArray', function fnDescribe() {
+describe('callFn', function fnDescribe() {
     it('should call functions correctly', function fnIt() {
         const testFn1 = jest.fn();
         callFn(testFn1);
         expect(testFn1).toHaveBeenCalled();
-
-        const testFn2 = jest.fn().mockImplementation((test) => test);
-        callFn(testFn2, 'foo');
-        expect(testFn2).toHaveBeenCalled();
-        expect(testFn2).toHaveBeenCalledWith('foo');
     });
     it('should be fail safe', function fnIt() {
         callFn('foo');
